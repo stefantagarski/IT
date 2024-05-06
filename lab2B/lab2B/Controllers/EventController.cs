e9a790ef39d949328bf2572c0f0bfc9aff78f13c
Stefan Tagarski <stefantagarski@gmail.com>
2024-05-07 00:43:02 +0200

    push to main

---
+++ lab2B/lab2B/Controllers/EventController.cs
@@ -0,0 +1,76 @@
+﻿using lab2B.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab2B.Controllers
+{
+    public class EventController : Controller
+    {
+        private static List<EventModel> events = new List<EventModel>();
+
+        public ActionResult ShowAllEvents()
+        {
+            return View(events);
+        }
+
+        public ActionResult AddEvent() {
+            EventModel model = new EventModel();
+
+            return View(model);
+        }
+
+        public ActionResult ShowInformations()
+        {
+            EventModel model = new EventModel();
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public ActionResult CreateNewEvent(EventModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("AddEvent", model);
+            }else
+            {
+                events.Add(model);
+                return View("ShowInformations", model);
+            }
+        }
+
+        public ActionResult DeleteEvent(int id)
+        {
+            events.RemoveAt(id);
+            return View("ShowAllEvents", events);
+        }
+
+        public ActionResult EditEvent(int id)
+        {
+           var model = events.ElementAt(id);
+            return View("EditEvent", model);
+        }
+
+        [HttpPost]
+        public ActionResult EditEvent(EventModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EditEvent", model);
+            }
+            else
+            {
+                var forUpdate = events.ElementAt(model.ID);
+                forUpdate.Name = model.Name;
+                forUpdate.Location = model.Location;
+
+                return View("ShowAllEvents", events);
+            }
+        }
+
+    }
+}
---
+++ lab2B/lab2B/Models/EventModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lab2B.Models
+{
+    public class EventModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "The location name must be between 5 and 30 characters", MinimumLength = 5)]
+        public string Location { get; set; }
+    }
+}
